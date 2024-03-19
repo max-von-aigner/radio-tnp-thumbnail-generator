959d69a293c95aa07a2ae44e4734aa5b6c11ca5a
Max-von-Aiger <von.aigner@gmail.com>
2024-03-19 20:54:39 +0100

    Fix build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // This condition checks if the configuration already has an `externals` property
+      // and preserves it, alongside adding the new rule for `canvas`
+      if (Array.isArray(config.externals)) {
+        config.externals.push("commonjs canvas");
+      } else if (typeof config.externals === "function") {
+        const originalExternals = config.externals;
+        config.externals = async (...args) => {
+          const originalResult = await originalExternals(...args);
+          return [...originalResult, "commonjs canvas"];
+        };
+      } else {
+        // If there are no `externals` defined, it initializes it with the `canvas` configuration
+        config.externals = ["commonjs canvas"];
+      }
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
