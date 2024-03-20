e04086488aad73d861728c85070aba0971b9e2ae
Max-von-Aiger <von.aigner@gmail.com>
2024-03-20 22:07:22 +0100

    New next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config, { webpack }) => {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
+  reactStrictMode: false,
+  webpack: (config) => {
     config.externals.push({
       sharp: "commonjs sharp",
       canvas: "commonjs canvas",
     });
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-        process: "process/browser",
-      })
-    );
     return config;
   },
 };
