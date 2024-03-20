77cc0305dfeb9d3f62c1f1df77de9404e2eb179f
Max-von-Aiger <von.aigner@gmail.com>
2024-03-20 21:38:30 +0100

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,25 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // This example targets avoiding bundling the 'canvas' package
-    // for environments where it's not compatible or needed.
-    if (!isServer) {
-      // Prevent webpack from bundling 'canvas' on the client side
-      config.externals.push({ canvas: "commonjs canvas" });
-    }
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, { webpack }) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    config.externals.push({
+      sharp: "commonjs sharp",
+      canvas: "commonjs canvas",
+    });
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: "process/browser",
+      })
+    );
     return config;
   },
 };
-
 export default nextConfig;
-
-// PREVIOUS CONFIG:
-
-//   /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
